5aa55879dee90fd29b5a69d9c34557379386420a
Daniel <Daniel@DESKTOP-QOTOO4B>
2023-06-10 23:47:18 -0700

    Se guarda y validan los datos

--- WebProspectos/App_Start/RouteConfig.cs
+++ WebProspectos/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WebProspectos
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Prospectos", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- WebProspectos/Controllers/ProspectosController.cs
+++ WebProspectos/Controllers/ProspectosController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using WebProspectos.Models;
 using WebProspectos.Models.TableViewModels;
+using WebProspectos.Models.ViewModels;
 
 namespace WebProspectos.Controllers
 {
@@ -27,6 +28,44 @@ namespace WebProspectos.Controllers
                                }).ToList();
             }
             return View(lsProspectos);
+        }
+        [HttpGet]
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Nuevo(ProspectoViewModel objProspectos)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(objProspectos);
+            }
+            using (var db = new WebContext())
+            {
+                Persona objPersona = new Persona()
+                {
+                    Nombre = objProspectos.Nombre,
+                    PrimerApellido = objProspectos.PrimerApellido,
+                    SegundoApellido = objProspectos.SegundoApellido,
+                    Direcciones = new List<Direccion>()
+                    {
+                        new Direccion() 
+                        {
+                            Calle = objProspectos.Calle,
+                            Numero = objProspectos.Numero,
+                            Colonia = objProspectos.Colonia,
+                            CodigoPostal = objProspectos.CodigoPostal
+                        }
+                    },
+                    Telefono = objProspectos.Telefono,
+                    Rfc = objProspectos.Rfc,
+                    Estatus = "Activo"
+                };
+                db.Personas.Add(objPersona);
+                db.SaveChanges();
+            }
+            return Redirect(Url.Content("~/Prospectos/"));
         }
     }
 }
