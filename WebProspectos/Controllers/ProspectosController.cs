e5f680b855ed7a228d3bd0708a42b9f61acd25a8
Daniel <Daniel@DESKTOP-QOTOO4B>
2023-06-10 17:25:07 -0700

    Antes de agregar boton para agregar prospecto

---
+++ WebProspectos/Controllers/ProspectosController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebProspectos.Models;
+using WebProspectos.Models.TableViewModels;
+
+namespace WebProspectos.Controllers
+{
+    public class ProspectosController : Controller
+    {
+        // GET: Prospectos
+        public ActionResult Index()
+        {
+            List<TablaProspectosViewModel> lsProspectos = null;
+            using (WebContext db = new WebContext())
+            {
+                lsProspectos = (from d in db.Personas
+                               select new TablaProspectosViewModel
+                               {
+                                   Id = d.Id,
+                                   Nombre = d.Nombre,
+                                   PrimerApellido = d.PrimerApellido,
+                                   SegundoApellido = d.SegundoApellido,
+                                   Estatus = d.Estatus
+                               }).ToList();
+            }
+            return View(lsProspectos);
+        }
+    }
+}
---
+++ WebProspectos/Models/TableViewModels/TablaProspectosViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebProspectos.Models.TableViewModels
+{
+    public class TablaProspectosViewModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public string Estatus { get; set; }
+    }
+}
